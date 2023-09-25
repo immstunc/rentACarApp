a3d43a9eb464517986d1198aa58eb53d9da01d76
salvation <merveselva@icloud.com>
2023-09-25 23:58:02 +0300

    rentACarApp

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+// Kullanıcı Listesi ve Araba Listesi  ve Rent A Car listesi olacak
+
+// Kullanıcı için
+
+// Name
+
+// Surname
+
+// Age
+
+// Licance
+
+
+
+// Car için
+
+// Model
+
+// Marka
+
+// Renk
+
+// Her eklenen kullanıcı class nesnesi ile bir listeye eklenecek aynı şekilde araçlar içinde aynı şey olacak
+
+// daha sonra rent acar işlemi için User ve Car için select option olarak listelenecek ve listeden araç ve kullanıcı seçilerek araç kiralanacak ve bu da Ren a car listesine eklenerek bir tabloya eklenecek
+
+class Kullanıcı{
+    Ad;
+    Soyad;
+    Age;
+    Licance;
+}
+
+
+class Car{
+    Model
+    Marka
+    Renk
+    
+}
+
+// value = row.children[0].children[3].children[0].textContent="Todo Listesi Başlığı Değişti"
